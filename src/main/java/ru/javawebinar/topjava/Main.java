0ab70f8cf2d864223e64983f42a6e1b867d6d44e
Samusia <stoker85@gmail.com>
2015-11-26 22:51:01 +0300

    First commit/2

---
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava;
+
+/**
+ * Created by Samusia
+ * on 26.11.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello Topjava Enterprise!");
+    }
+}
