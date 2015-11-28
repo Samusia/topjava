9be4d96c542a9aafffce90c53bac2c3a2fcb3aef
Samusia <stoker85@gmail.com>
2015-11-28 20:47:06 +0300

    Add lambda samples

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,4 +1,7 @@
+
 package ru.javawebinar.topjava;
+
+import java.util.function.Consumer;
 
 /**
  * Created by Samusia
@@ -6,6 +9,20 @@ package ru.javawebinar.topjava;
  */
 public class Main {
     public static void main(String[] args) {
+        execute(() -> System.out.println("Hello Topjava!"));
+        consume(System.out::println, "Hello Topjava!");
+    }
+
+    private static void execute(Runnable runnable) {
+        System.out.println("Start runner");
+        runnable.run();
+        System.out.println("End runner");
+    }
+
+    private static <String> void consume(Consumer<String> consumer, String out) {
+        System.out.println("Start consume");
+        consumer.accept(out);
+        System.out.println("End consume");
         System.out.format("Hello Topjava Enterprise!");
     }
 }
