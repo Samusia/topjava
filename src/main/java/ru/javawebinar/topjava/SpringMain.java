e60116b43aef899c12ebc5029ae9b6610173852b
Samusia <stoker85@gmail.com>
2015-11-30 20:51:05 +0300

    Add dependency injection

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,7 +2,9 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.UserService;
 
 import java.util.Arrays;
 
@@ -14,8 +16,8 @@ public class SpringMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
         System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
-        UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
-        userRepository = appCtx.getBean(UserRepository.class);
+        UserService userService = appCtx.getBean(UserService.class);
+        System.out.println(userService.save(new User(1, "userName", "email", "password", Role.ROLE_ADMIN)));
         appCtx.close();
     }
 }
