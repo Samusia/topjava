e65bd83eacd30e9fead276979d575ecbadb6f1e6
Samusia <stoker85@gmail.com>
2015-12-01 22:17:31 +0300

    Populate and init db

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,13 @@
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+-- password
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password');
+-- admin
+INSERT INTO users (name, email, password)
+VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES ('ROLE_USER', 100000);
+INSERT INTO user_roles (role, user_id) VALUES ('ROLE_ADMIN', 100001);
