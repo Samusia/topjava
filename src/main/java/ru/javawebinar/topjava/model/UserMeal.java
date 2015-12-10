de37dda82643c9e50367a6352848b7da0d875d7a
Samusia <stoker85@gmail.com>
2015-12-10 21:15:26 +0300

    HW4

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,22 +1,47 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 /**
  * Created by Samusia
  * on 27.11.2015.
  */
-public class UserMeal extends BaseEntity {
+@NamedQueries({
+        @NamedQuery(name = UserMeal.GET, query = "SELECT m FROM UserMeal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = UserMeal.ALL_SORTED, query = "SELECT m FROM UserMeal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = UserMeal.DELETE, query = "DELETE FROM UserMeal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = UserMeal.GET_BETWEEN,
+                query = "SELECT m from UserMeal m WHERE m.user.id=:userId " +
+                        " AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC"),
 
+//        @NamedQuery(name = UserMeal.UPDATE, query = "UPDATE UserMeal m SET m.dateTime = :datetime, m.calories= :calories," +
+//                "m.description=:desc where m.id=:id and m.user.id=:userId")
+})
+@Entity
+@Table(name = "meals")
+public class UserMeal extends BaseEntity {
+    public static final String GET = "UserMeal.get";
+    public static final String ALL_SORTED = "UserMeal.getAll";
+    public static final String DELETE = "UserMeal.delete";
+    public static final String GET_BETWEEN = "UserMeal.getBetween";
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     protected LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotEmpty
     protected String description;
 
+    @Column(name = "calories")
     protected int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public UserMeal() {
@@ -63,6 +88,14 @@ public class UserMeal extends BaseEntity {
 
     public void setCalories(int calories) {
         this.calories = calories;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
