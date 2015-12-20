3ea1f41e2f771d199fe8a6a31c307dadc8e4f490
Samusia <stoker85@gmail.com>
2015-12-20 22:44:37 +0300

    Add login form

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,25 +1,25 @@
-package ru.javawebinar.topjava.web.user;
+package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
-import ru.javawebinar.topjava.web.AbstractControllerTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Created by Samusia
- * on 15.12.2015.
- */
-public class UserControllerTest extends AbstractControllerTest {
+
+public class RootControllerTest extends AbstractControllerTest {
 
     @Test
     public void testUserList() throws Exception {
         mockMvc.perform(get("/users"))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("userList"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/userList.jsp"));
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("userList"))
+//                .andExpect(forwardedUrl("/WEB-INF/jsp/userList.jsp"));
 //                .andExpect(model().attribute("userList", hasSize(2)))
 //                .andExpect(model().attribute("userList", hasItem(
 //                        allOf(
@@ -27,5 +27,9 @@ public class UserControllerTest extends AbstractControllerTest {
 //                                hasProperty("name", is(USER.getName()))
 //                        )
 //                )));
+    }
+
+    @Test
+    public void testMealList() throws Exception {
     }
 }
