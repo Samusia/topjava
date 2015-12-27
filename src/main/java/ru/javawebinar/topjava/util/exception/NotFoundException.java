8f8426d9383aa64a90c825fd084b3540095a0e66
Samusia <stoker85@gmail.com>
2015-12-27 16:37:35 +0300

    Add not found 404

--- src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
@@ -1,9 +1,13 @@
 package ru.javawebinar.topjava.util.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by Samusia
  * on 30.11.2015.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No data found")  // 404
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
